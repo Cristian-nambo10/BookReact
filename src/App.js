ad6c6e780a0e5110b01cb9823c783473150d263a
Cristian Nambo <nambocristian10@gmail.com>
2022-07-20 02:03:42 -0500

    wip: sort price functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import Books from "./pages/Books";
+import { books } from "./data"
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
       <Route />
       <Nav />
       <Route path='/' exact component={Home} />
-      <Route path='/books' component={Books} />
+      <Route path='/books' render={() => <Books books={books} />}/>
       <Footer />
     </div>
     </Router>
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Book from '../components/ui/Book'
-import { books } from "../data"
 
-function Books() {
+function Books({ books: initialBooks }) {
+    const [books, setBooks] = useState(initialBooks);
+
+    function filterBooks(filter) {
+        // console.log(filter)
+        if (filter === "LOW_TO_HIGH") {
+            setBooks(books.slice().sort((x, y) => (x.salePrice || x.originalPrice) - (y.salePrice || y.originalPrice)))
+        }
+        if (filter === "HIGH_TO_LOW") {
+            setBooks(books.slice().sort((x, y) => (y.salePrice || y.originalPrice) - (x.salePrice || x.originalPrice)))
+        }
+    }
   return (
     <div id="books__body">
         <main id="books__main">
@@ -10,7 +20,7 @@ function Books() {
                 <div className="row">
                     <div className="books__header">
                         <h2 className="section__title books__header--title">All Books</h2>
-                        <select id="filter" defaultValue="DEFAULT">
+                        <select id="filter" defaultValue="DEFAULT" onChange={(event) => filterBooks(event.target.value)}>
                             <option value="DEFAULT" disabled>Sort</option>
                             <option value="LOW_TO_HIGH">Price, Low to High</option>
                             <option value="HIGH_TO_LOW">Price, High to Low</option>
